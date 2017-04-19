d6e85c9f1dc139ec4a90d65c37e5f06721430b8a
Slogg <Dmitry.Ionas@gmail.com>
2017-04-19 15:52:33 +0300

    create Universe.cs create class with the logic of the universe

---
+++ GameOfLife/GameOfLife/Universe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public enum StatusLiveCells { Dead = 0, Live = 1 }
+
+    class Universe
+    {
+        public int countColumns;
+        public int countRows;
+        public int countLiveCells;
+        public StatusLiveCells[,] plane;
+        void ClearBoard()
+        {
+            Array.Clear(plane, 0, countRows * countColumns);
+        }
+
+        void UpdateBoard()
+        {
+            
+        }
+    }
+}
